9540649bbd7bdd0a4bc2bf70932bf7ce01ed9838
Gaganjeet Bhardwaj <gaganbhardwaj1529@gmail.com>
2024-06-03 21:41:05 -0400

    New Feature added in vite project

---
+++ reactGagan/src/newFeature.jsx
@@ -0,0 +1,12 @@
+// src/NewFeature.jsx
+import React from 'react';
+
+const NewComponent = () => {
+    return (
+        <div>
+            <h1>Hello Gaganjeet Bhardwaj</h1>
+        </div>
+    );
+};
+
+export default NewComponent;
